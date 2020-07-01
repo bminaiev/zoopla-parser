c58fcdc46c54bd17ef201511890ba3412bb147d1
Borys Minaiev <bminaiev@gmail.com>
2020-07-01 21:12:11 +0100

    get photos

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,6 @@
 import org.jsoup.Jsoup
 import java.io.File
+import java.io.IOException
 import java.net.URL
 
 const val BASE_ADDRESS = "https://www.zoopla.co.uk";
@@ -13,17 +14,32 @@ fun makeGoodFilename(filename: String): String {
     return filename.replace("[^a-zA-Z0-9.\\-]".toRegex(), "_")
 }
 
-fun sendQuery(url: String, useCache: Boolean = true): String {
+fun sendQuery(url: String, useCache: Boolean = true): String? {
     val fileCache = File(CACHE_DIR + makeGoodFilename(url))
     if (useCache && fileCache.exists()) {
-        return fileCache.readText()
+        val cachedRes = fileCache.readText()
+        if (cachedRes.isEmpty()) {
+            return null;
+        }
+        return cachedRes;
     }
-    val response = URL(url).readText()
-    fileCache.writeText(response)
-    return response
+    return try {
+        val response = URL(url).readText()
+        fileCache.writeText(response)
+        response
+    } catch (e: IOException) {
+        fileCache.writeText("");
+        null;
+    }
 }
 
 fun buildLink(relative: String) = BASE_ADDRESS + relative
+
+fun getPhotosLink(id: Int) = "https://www.zoopla.co.uk/to-rent/details/photos/$id"
+
+fun getIdFromLink(link: String): Int {
+    return link.substring(link.lastIndexOf('/') + 1).toInt();
+}
 
 fun parseMonthPrice(s: String): Int? {
     if (s[0] != '£') {
@@ -36,7 +52,6 @@ fun parseMonthPrice(s: String): Int? {
 }
 
 fun handleResponse(response: String) {
-
     val properties = Jsoup.parse(response).select(LISTINGS_CLASS)
 
     val links = properties.map {
@@ -47,14 +62,24 @@ fun handleResponse(response: String) {
 
     val firstLink = buildLink(links[0])
 
-    links.forEach {
-        val link = buildLink(it)
+    links.forEach { linkIt ->
+        val link = buildLink(linkIt)
         System.err.println("Send query $link")
-        val propery = sendQuery(link)
-        val priceStr = Jsoup.parse(propery).selectFirst(PRICE_CLASS).text()
+        val propertyId = getIdFromLink(link)
+        val property = sendQuery(link)
+        val priceStr = Jsoup.parse(property).selectFirst(PRICE_CLASS).text()
         val pricePoundsPerMonth = parseMonthPrice(priceStr)
 
         System.err.println("price (pounds / month) = $pricePoundsPerMonth")
+
+        val photosPage = sendQuery(getPhotosLink(propertyId))
+        if (photosPage != null) {
+            val photos = Jsoup.parse(photosPage).getElementsByTag("img").filter {
+                !it.attr("style").isEmpty()
+            }.map { it.attr("src") }
+        }
+
+
     }
 
     println(firstLink)
@@ -64,7 +89,7 @@ fun handleResponse(response: String) {
 
 fun sendRequest() {
     val url = BASE_ADDRESS + "/to-rent/property/london/britton-street/ec1m-5ny/?added=24_hours&include_shared_accommodation=false&price_frequency=per_month&q=ec1m%205ny&radius=1&results_sort=newest_listings&search_source=home&page_size=100"
-    val response = sendQuery(url)
+    val response = sendQuery(url)!!
     handleResponse(response)
 }
 
