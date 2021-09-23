374d88c21b231f2a5a9b8b8ffaf1a78918482bf1
Borys <mb.787788@gmail.com>
2021-09-23 22:47:58 +0100

    fix zoopla parsing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import kotlinx.serialization.UnstableDefault
+import kotlinx.serialization.json.Json
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.insert
@@ -8,17 +9,17 @@ import org.jsoup.Jsoup
 import java.net.URL
 
 const val BASE_ADDRESS = "https://www.zoopla.co.uk";
+const val BASE_CDN = "https://lc.zoocdn.com/"
 
-private const val LISTINGS_CLASS = ".listing-results-price"
-private const val PRICE_CLASS = ".ui-pricing__main-price"
-private const val FLOOR_CLASS = ".ui-modal-floorplan__wrap"
-private const val GALERY_CLASS = ".ui-modal-gallery__asset--center-content"
-private const val SUMMARY_CLASS = ".dp-sidebar-wrapper__summary"
-private const val ADDRESS_CLASS = ".ui-property-summary__address"
+private const val LISTINGS_CLASS = "a[data-testid=listing-details-link]"
+private const val PRICE_CLASS = "span[data-testid=price]"
+private const val ADDRESS_CLASS = "span[data-testid=address-label]"
+private const val NEXT_DATA_CLASS = "script[id=__NEXT_DATA__]"
 
 fun getPhotosLink(id: Int) = "https://www.zoopla.co.uk/to-rent/details/photos/$id"
 
 private fun getIdFromLink(link: String): Int {
+    val link = link.substring(0, link.length - 1);
     return link.substring(link.lastIndexOf('/') + 1).toInt();
 }
 
@@ -37,7 +38,7 @@ private object seen_properties : Table() {
 }
 
 fun buildPropertyLink(id: Int): String {
-    return "https://www.zoopla.co.uk/to-rent/details/$id"
+    return "https://www.zoopla.co.uk/to-rent/details/$id/"
 }
 
 fun convertOneProperty(propertyId: Int, queryParams: QueryParams, config: Config): Property? {
@@ -54,14 +55,23 @@ fun convertOneProperty(propertyId: Int, queryParams: QueryParams, config: Config
         Logger.println("Skip property because of price $pricePoundsPerMonth")
         return null
     }
-    val address = Address(parsedHTML.select(SUMMARY_CLASS).select(ADDRESS_CLASS).text())
-    val floor = parsedHTML.selectFirst(FLOOR_CLASS)?.selectFirst(GALERY_CLASS)?.attr("style")
-    if (floor == null) {
+    val address = Address(parsedHTML.select(ADDRESS_CLASS).text())
+    val nextData = parsedHTML.selectFirst(NEXT_DATA_CLASS).html()
+    val nextDataJson = Json.parseJson(nextData)
+    val curPropertyJson = nextDataJson.jsonObject.getObject("props").getObject("pageProps").getObject("data").getObject("listing")
+    val floorPlan = curPropertyJson.getObject("floorPlan")
+
+    val image = floorPlan["image"]
+    if (image == null || image.isNull || image.jsonArray.size == 0) {
         Logger.println("Skip property because no floor plan")
-        return null
+
+        return null;
     }
-    val regex = "background-image: url\\('(.*)'\\)".toRegex()
-    val (floorPlanImage) = regex.find(floor)!!.destructured
+
+    val images = image.jsonArray
+    val imageFilename = images[0].jsonObject.getPrimitive("filename").content
+    val floorPlanImage = BASE_CDN + imageFilename
+
     val areaSqM = FloorPlanOCR.loadImageAndParseSqM(config, URL(floorPlanImage))
     val photosPage = Utils.sendQuery(getPhotosLink(propertyId))
     if (photosPage == null) {
@@ -88,7 +98,8 @@ fun convertOneProperty(propertyId: Int, queryParams: QueryParams, config: Config
 }
 
 fun handleResponse(response: String, telegram: Telegram, config: Config, queryParams: QueryParams) {
-    val properties = Jsoup.parse(response).select(LISTINGS_CLASS)
+    val parsed = Jsoup.parse(response);
+    val properties = parsed.select(LISTINGS_CLASS)
 
     val links = properties.map {
         it.attr("href")
@@ -150,12 +161,7 @@ fun sendRequest(telegram: Telegram, config: Config) {
     )
     val allQueryParams =
         listOf(
-            queryParamsNear,
-            queryParamsAngel,
-            queryParamsFacebook,
-            queryParamsKingsCross,
-            queryParamsHampstead,
-            queryNearHydePark
+            queryParamsAngel
         )
     allQueryParams.forEach {
         Logger.println("Handle query with tag = " + it.tag)
@@ -180,6 +186,7 @@ fun main(args: Array<String>) {
         Logger.println("Start!")
         sendRequest(telegram, config)
         Logger.println("Check right move also!")
-        RightMove.getNewPropertiesAndSendUpdates(config, telegram)
+        // TODO: fix parsing here...
+//        RightMove.getNewPropertiesAndSendUpdates(config, telegram)
     }
 }
